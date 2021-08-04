eefaddff4e942a5f035a562d639c224892fc452c
FengYarnom <fengyarnom@gmail.com>
2021-08-04 21:50:55 +0800

    [version][1.1] 初步完成了Flask的部署，网站正在尝试后端的工作

--- app/app.py
+++ app/app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app.db'
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+# Router
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    dict = {
+        'hello':'Hello'
+    }
+    return render_template('index.html',dict=dict)
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+# main function
+if __name__ == '__main__':
+    # db.create_all()
+    app.run(debug=True,host='0.0.0.0',port=3000)
